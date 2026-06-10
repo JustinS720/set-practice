8244c8fbd2925959d851438f1a557e995fb37eb2
JustinS720 <274596103+JustinS720@users.noreply.github.com>
2026-06-10 13:31:06 -0700

    Added UniqueCharacterChecker at O(n)

--- src/UniqueCharacterChecker.java
+++ src/UniqueCharacterChecker.java
@@ -1,3 +1,6 @@
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * The UniqueCharacterChecker class provides a method to check if all characters
  * in a given word are unique.
@@ -14,13 +17,22 @@ public class UniqueCharacterChecker {
         // TODO: implement this!
         // Requirement: This must run in O(n) time, where n is the number of characters in the word
         // Hint: Stuck? Consider looking up "charAt" and seeing how it can help you
+        Set<Character> current = new HashSet<>();
+        for (int i = 0; i < word.length(); i++){
+            current.add(word.charAt(i));
+        }
+
+        if (current.size() == word.length()) {
+            return true;
+        }
+
         return false;
     }
 
     public static void main(String[] args) {
-        String word1 = "hello";
-        String word2 = "world";
-        String word3 = "abcdefg";
+        String word1 = "hello"; //false
+        String word2 = "world"; //true
+        String word3 = "abcdefg"; //true
 
         System.out.println(word1 + " has unique characters: " + hasUniqueCharacters(word1));
         System.out.println(word2 + " has unique characters: " + hasUniqueCharacters(word2));
